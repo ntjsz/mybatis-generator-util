e13c11d0940d495095b4a8a9e296deba3d6f1d0a
huhaitao <huhaitao@tianyancha.com>
2021-01-21 18:13:03 +0800

    add plugin

---
+++ src/main/java/CustomizedMybatisPlugin.java
@@ -0,0 +1,53 @@
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.PluginAdapter;
+import org.mybatis.generator.api.dom.java.Field;
+import org.mybatis.generator.api.dom.java.JavaReservedWords;
+import org.mybatis.generator.api.dom.java.Method;
+import org.mybatis.generator.api.dom.java.TopLevelClass;
+
+import java.util.List;
+
+/**
+ * add lombok
+ * ignore getter setter
+ * ignore java reserved words
+ */
+public class CustomizedMybatisPlugin extends PluginAdapter {
+    @Override
+    public boolean validate(List<String> warnings) {
+        return true;
+    }
+
+    @Override
+    public boolean modelBaseRecordClassGenerated(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+        topLevelClass.addImportedType("lombok.Data");
+
+        topLevelClass.addAnnotation("@Data");
+        return true;
+    }
+
+    @Override
+    public boolean modelSetterMethodGenerated(Method method, TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn, IntrospectedTable introspectedTable, ModelClassType modelClassType) {
+        return false;
+    }
+
+    @Override
+    public boolean modelGetterMethodGenerated(Method method, TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn, IntrospectedTable introspectedTable, ModelClassType modelClassType) {
+        return false;
+    }
+
+    @Override
+    public boolean modelFieldGenerated(Field field, TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn, IntrospectedTable introspectedTable, ModelClassType modelClassType) {
+        if (field != null) {
+            String name = field.getName();
+            if (JavaReservedWords.containsWord(name)) {
+                String columnName = introspectedColumn.getActualColumnName();
+                String tableName = introspectedTable.getFullyQualifiedTable().toString();
+                System.err.println("JavaReservedWord is detected and ignored. table.column = " + tableName + "." + columnName);
+                return false;
+            }
+        }
+        return true;
+    }
+}
